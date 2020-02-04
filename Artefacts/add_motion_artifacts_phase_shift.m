b3c3368001e0d35c5e600d821c8f3f9c99be76ca
Jose Bernal <jose.bernal@correounivalle.edu.co>
2020-02-04 04:51:56 +0000

    Delete add_motion_artifacts_phase_shift.m

--- Artefacts/add_motion_artifacts_phase_shift.m
+++
@@ -1,6 +0,0 @@
-function k_space = add_motion_artifacts_phase_shift(k_space, probability_missing_line)
-   phase = angle(k_space);
-   magnitude = abs(k_space);
-   
-   k_space = magnitude.*exp(1i*phase-1i*pi/4); % -pi/4 shift
-end
