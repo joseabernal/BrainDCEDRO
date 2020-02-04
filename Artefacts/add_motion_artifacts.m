27777d73ef4476a700b93e2e11c098c8243217af
Jose Bernal <jose.bernal@correounivalle.edu.co>
2020-02-04 04:51:46 +0000

    Delete add_motion_artifacts.m

--- Artefacts/add_motion_artifacts.m
+++
@@ -1,9 +0,0 @@
-function k_space = add_motion_artifacts(k_space, probability_missing_line)
-    zeroed_lines = rand(size(k_space, 1), 1, size(k_space, 3)) < probability_missing_line;
-    
-    disp(sum(zeroed_lines(:)))
-    
-    zeroed_lines = repmat(zeroed_lines, [1, size(k_space, 2), 1]);
-
-    k_space(zeroed_lines) = 0;
-end
