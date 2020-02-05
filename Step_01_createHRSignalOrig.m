3f27f274b7f06c31cc57d8341d638a8655db3020
mjt320 <mjt320@googlemail.com>
2020-02-05 14:20:56 +0000

    mjt comments

--- Step_01_createHRSignalOrig.m
+++ Step_01_createHRSignalOrig.m
@@ -1,10 +1,13 @@
 %% Create high resolution signal
-%  This script allows creating a 4D high resolution DCE-MRI acquisition
+%  This script allows creation of a 4D high resolution DCE-MRI signal
 %  given specific imaging parameters and a head atlas. 
 %
 %  This function takes a high resolution atlas and imaging parameters, 
 %  generates parameter maps, creates concentration-time curves using the
 %  Patlak model, and creates signal-time curves using the SPGR model.
+%
+%  For tissues not described by Patlak model parameters (i.e. non-brain), representative signal-time data is
+%  determined directly from in-vivo measurements.
 %  
 % (c) Jose Bernal and Michael J. Thrippleton 2019
 
@@ -34,7 +37,7 @@ save_scan({HR_PS_perMin, HR_vP, HR_Ct_mM}, ...
 % generate high res nonbrain signal
 HR_SI_nonbrain = generateHRNonBrainSignal(HR_tissue_map, SI_nonbrain, NTrue, NFrames);
 
-% generate high res image data
+% generate high res signal in brain and combine with non-brain signal
 HR_SI = generateHRSignal(...
     HR_Ct_mM, HR_tissue_map, HR_SI_nonbrain, M0, T10_s, T2s0_s, FA_deg, TR_s, TE_s, ...
     r1_perSpermM, r2_perSpermM, NTrue);
